07441385f34262887a809eac6d605eda4ec315c0
michellechung099 <mchung099@gmail.com>
2024-08-08 13:41:43 -0700

    change protobuf version

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ def main():
                         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
                         pdf_display = f"""<embed src="data:application/pdf;base64,{base64_pdf}"
-                            width="100%" height="900" type="application/pdf"></embed>"""
+                            width="100%" height="900" type="application/pdf">"""
                     
                         st.markdown(pdf_display, unsafe_allow_html=True)
 
